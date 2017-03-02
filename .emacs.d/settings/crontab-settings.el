1f7d57f57a186afd56ff432bc486d014ebd477a8
James Nowell <jnowell@savi.com>
2017-03-02 10:04:38 -0500

    auto crontab mode

--- .emacs.d/settings/crontab-settings.el
+++ .emacs.d/settings/crontab-settings.el
@@ -1,3 +1,5 @@
 
 (require 'crontab-mode)
+(autoload 'crontab-mode "crontab-mode" "Mode for editing crontab files" t)
+(add-to-list 'auto-mode-alist '("cron\\(tab\\)?\\."    . crontab-mode))
 (provide 'crontab-settings)
